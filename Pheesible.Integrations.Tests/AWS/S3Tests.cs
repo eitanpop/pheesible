6a26f9713c929da6d5df1d9fac5a469220ab55e1
Eitan <eitan@thewarehouseco.com>
2020-09-27 12:55:13 -0600

    added s3 integration

---
+++ Pheesible.Integrations.Tests/AWS/S3Tests.cs
@@ -0,0 +1,20 @@
+﻿using Pheesible.Integrations.AWS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pheesible.Integrations.Tests.AWS
+{
+    public class S3Tests
+    {
+        [Fact]
+        public async Task GetS3Item()
+        {
+            S3 s3 = new S3();
+            byte[] byteArr = await s3.GetObject("protected/us-east-1:a0789da5-d0ae-4689-af4f-6356b95d689a/1/ad/Okta.png");
+            Console.WriteLine("works");
+        }
+    }
+}
---
+++ Pheesible.Integrations/AWS/S3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace Pheesible.Integrations.AWS
+{
+    public class S3
+    {
+        public async Task<byte[]> GetObject(string bucketName, string key)
+        {
+            var s3 = new AmazonS3Client();
+
+            GetObjectRequest request = new GetObjectRequest
+            {
+                BucketName = bucketName,
+                Key = key
+            };
+            using GetObjectResponse response = await s3.GetObjectAsync(request);
+            await using var memoryStream = new MemoryStream();
+            response.ResponseStream.CopyTo(memoryStream);
+            return memoryStream.ToArray();
+        }
+    }
+}
